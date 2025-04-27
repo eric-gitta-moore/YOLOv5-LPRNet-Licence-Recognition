de670323eef00ada3c23cc2275ba499e067d018c
Eric Moore <EricGittaMoore@duck.com>
2025-04-28 00:24:20 +0800

    修改训练脚本

--- scripts/train_lprnet.sh
+++ scripts/train_lprnet.sh
@@ -2,4 +2,5 @@ export PYTHONPATH=`pwd`
 python tools/train_lprnet.py \
     --train_img_dirs /home/linux/workspace/ccpd2019/images/train \
     --test_img_dirs /home/linux/workspace/ccpd2019/images/test \
-    --pretrained_model weights/lprnet_best.pth
+    --pretrained_model weights/lprnet_best.pth \
+    --num_workers 10
--- scripts/train_yolo.sh
+++ scripts/train_yolo.sh
@@ -3,5 +3,5 @@ export PYTHONPATH=`pwd`
 python tools/train_yolov5.py \
     --cfg models/yolov5s.yaml \
     --data data/ccpd.yaml \
-    # --weights weights/yolov5s.pt \
+    --weights weights/yolov5_best.pt \
     --device 0
